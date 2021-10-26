24c1410a3f7d35ed1eb562be39c3c63d814493a7
Joseph Renolayan <jrenolayan@gmail.com>
2021-10-25 21:58:43 -0600

    product details working

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -31,7 +31,7 @@ const LoginContainer = ({ onLoginSuccess }) => {
 	const handleLoginPress = async () => {
 		try {
 			const response = await axios.post('http://caliboxs.com/api/v1/login', {
-				email: 'e@e.com',
+				email: 'test@test.com',
 				password: '123456',
 			})
 
--- src/store/actions/tasks.js
+++ src/store/actions/tasks.js
@@ -116,7 +116,6 @@ export const initToDo = (callback = () => null) => {
 
 			// console.log('this is filtered products  ::', filteredProducts, '\n')
 			// console.log('this is filtered Categories  ::', filteredCategories, '\n')
-			userFilteredCategories.forEach((cat) => console.log(`cat ID`, cat.name))
 			const updatedProducts = await setCategories(userFilteredProducts, userFilteredCategories)
 
 			callback(updatedProducts)
