02001ab0ff3a9c151bb8e7c61063c04d6afc8e29
Joseph Renolayan <jrenolayan@gmail.com>
2021-12-20 18:19:08 -0700

    edit/add category fix

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -39,6 +39,8 @@ const LoginContainer = ({ onLoginSuccess }) => {
 	const handleLoginPress = async () => {
 		try {
 			const response = await axios.post('http://caliboxs.com/api/v1/login', {
+				// email: 'testacct@test.com',
+				// password: '123456',
 				email,
 				password,
 			})
